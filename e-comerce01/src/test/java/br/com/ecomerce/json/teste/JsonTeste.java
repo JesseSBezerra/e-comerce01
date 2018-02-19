b0a4ba35ed412e2baa8f69499c8429c46f69ce01
JesseSBezerra <jessebezerra@hotmail.com.br>
2018-02-19 14:11:32 -0300

    Segundi commit
    Ajuste nas funções do json

--- e-comerce01/src/test/java/br/com/ecomerce/json/teste/JsonTeste.java
+++ e-comerce01/src/test/java/br/com/ecomerce/json/teste/JsonTeste.java
@@ -2,10 +2,10 @@ package br.com.ecomerce.json.teste;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 import javax.json.Json;
-import javax.json.JsonArray;
 import javax.json.JsonObject;
 import javax.json.JsonReader;
 
@@ -13,18 +13,23 @@ import javax.json.JsonReader;
 public class JsonTeste {
 
 public static void main(String[] args) throws IOException {
-	URL url = new URL("https://api.mercadolibre.com/sites/MLB/search?q=tv");
-	 try (InputStream is = url.openStream();
-	      JsonReader rdr = Json.createReader(is)) {
-	 
-	      JsonObject obj = rdr.readObject();
-	      JsonArray results = obj.getJsonArray("data");
-//	      for (JsonObject result : results.getValuesAs(JsonObject.class)) {
-//	          System.out.print(result.getJsonObject("from").getString("name"));
-//	          System.out.print(": ");
-//	         System.out.println(result.getString("message", ""));
-//	         System.out.println("-----------");
-//	     }
-	 }
+	try{
+		URL url = new URL("http://api.walmartlabs.com/v1/search?query=tv&format=json&apiKey=k4fkjg3q2n972dawavvkj7jd");
+		HttpURLConnection conn=(HttpURLConnection)url.openConnection();
+		conn.setRequestProperty("Content-Type", "application/json");
+		InputStream is = conn.getInputStream();
+		JsonReader rdr = Json.createReader(is);
+		JsonObject obj = rdr.readObject();
+		
+		javax.json.JsonArray array = obj.getJsonArray("items");
+		for(int i = 0; i < array.size(); i++){
+		System.out.println(array.getJsonObject(i).getString("name").replaceAll("\"", ""));	
+		}
+		System.out.println(obj);
+		
+
+	}catch(Exception e){
+		e.printStackTrace();
+	}
 }	
 }
